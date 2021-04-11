85058a8e29254747b5c4171508928989ac527638
Ashutosh Varma <ashutoshvarma11@live.com>
2021-04-11 15:33:54 +0530

    hardcode WETH balance

--- containers/WethContract.ts
+++ containers/WethContract.ts
@@ -19,7 +19,8 @@ function useContract() {
 
   const getTokenInfo = async () => {
     if (contract && address) {
-      const wethBalanceRaw: BigNumber = await contract.balanceOf(address);
+      // const wethBalanceRaw: BigNumber = await contract.balanceOf(address);
+      const wethBalanceRaw: BigNumber = 0;
       const wethBalance = utils.formatEther(wethBalanceRaw);
       setWethBalance(wethBalance);
     }
