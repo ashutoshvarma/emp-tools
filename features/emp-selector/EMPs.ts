26133fbc1ab0dad16d2e4fb6d95ec4cd87757aae
Ashutosh Varma <ashutoshvarma11@live.com>
2021-04-11 15:33:13 +0530

    add test EMPs

--- features/emp-selector/EMPs.ts
+++ features/emp-selector/EMPs.ts
@@ -1,8 +1,4 @@
 export const EMPs: { [networkId: number]: string[] } = {
-  1: [
-      "0xa42675322870Df548A53FCDb8b389Dd9033B84b7"
-  ],
-  2: [
-      "0x8c4394c5c1E997BD7cA25605D1c821Cbd37cF534"
-  ],
+  1666600000: ["0xa42675322870Df548A53FCDb8b389Dd9033B84b7"],
+  1666700000: ["0x8c4394c5c1E997BD7cA25605D1c821Cbd37cF534"],
 };
