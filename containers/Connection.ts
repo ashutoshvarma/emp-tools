14e0f5b474d4bcd6604d8b807a4d62070658a035
Ashutosh Varma <ashutoshvarma11@live.com>
2021-04-11 15:43:33 +0530

    add harmony testnet to supported network

--- containers/Connection.ts
+++ containers/Connection.ts
@@ -13,7 +13,7 @@ type Block = ethers.providers.Block;
 type Network = ethers.providers.Network;
 type Signer = ethers.Signer;
 
-const SUPPORTED_NETWORK_IDS: number[] = [1, 42];
+const SUPPORTED_NETWORK_IDS: number[] = [1666700000];
 
 function useConnection() {
   const [provider, setProvider] = useState<Provider | null>(null);
